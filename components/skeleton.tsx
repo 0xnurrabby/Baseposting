d00998fc3910c78d7cf0b2e34ce75e9962d82b04
NURRABBY SHUVO <106588083+0xnurrabby@users.noreply.github.com>
2026-01-08 20:37:57 +0600

    Delete components directory

--- components/skeleton.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { cn } from "@/lib/utils";
-
-export function Skeleton({ className }: { className?: string }) {
-  return <div className={cn("animate-pulse rounded-xl bg-zinc-200/70 dark:bg-zinc-800/70", className)} />;
-}
