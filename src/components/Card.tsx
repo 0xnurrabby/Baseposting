5eefae5f4f20c7742787801edfd9b30f39ef7729
NURRABBY SHUVO <106588083+0xnurrabby@users.noreply.github.com>
2026-01-08 18:48:14 +0600

    Delete src directory

--- src/components/Card.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Card({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="gradient-border shadow-soft">
-      <div className="card p-4 sm:p-5">{children}</div>
-    </div>
-  );
-}
